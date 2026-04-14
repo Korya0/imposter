c74519e25c4882c0a5e814401462e63f52bcb999
mahmoudmohamed <mahmoudmohamed01559@gmail.com>
2026-04-14 21:57:22 +0200

    add appstrings,app assets

---
+++ lib/core/constants/app_assets.dart
@@ -0,0 +1,24 @@
+abstract class AppSvgs {
+  static const String _baseSvg = 'assets/svgs';
+
+  static const String animals = '$_baseSvg/animals.svg';
+  static const String fingerprint = '$_baseSvg/fingerprint.svg';
+  static const String food = '$_baseSvg/Food.svg';
+  static const String incognito = '$_baseSvg/incognito.svg';
+  static const String logo = '$_baseSvg/logo.svg';
+  static const String paper = '$_baseSvg/paper.svg';
+  static const String peopleGroup = '$_baseSvg/people-group.svg';
+  static const String places = '$_baseSvg/Places.svg';
+  static const String play = '$_baseSvg/play.svg';
+  static const String professions = '$_baseSvg/Professions.svg';
+  static const String question = '$_baseSvg/question.svg';
+  static const String redo = '$_baseSvg/redo.svg';
+  static const String things = '$_baseSvg/Things.svg';
+  static const String timeOclock = '$_baseSvg/time-oclock.svg';
+}
+
+abstract class AppImages {
+  static const String _baseImage = 'assets/images';
+
+  static const String logoSplashIcon = '$_baseImage/logo_splash_icon.png';
+}
---
+++ lib/core/constants/app_strings.dart
@@ -0,0 +1,22 @@
+abstract class AppStrings {
+  static const String spy = 'الجاسوس';
+  static const String drawer = 'الدرج';
+  static const String howToPlay = 'أزاي تلعب';
+  static const String letsGo = 'يلا بينا';
+  static const String online = 'أونلاين';
+  static const String places = 'اماكن';
+  static const String food = 'اكل';
+  static const String professions = 'وظائف';
+  static const String animals = 'حيوانات';
+  static const String things = 'اشياء';
+  static const String numberOfPlayers = 'عدد الاعبين';
+  static const String numberOfSpies = 'عدد الجواسيس';
+  static const String numberOfMinutes = 'عدد الدقائق';
+  static const String start = 'ابدا';
+  static const String scanHere = 'ابصم هنا';
+  static const String redoRound = 'اعاده الدور';
+  static const String next = 'اللي بعده';
+  static const String youAreNotTheSpy = 'انت مش جاسوس';
+  static const String youAreTheSpy = 'انت الجاسوس';
+  static const String ready = 'كده كله تمام';
+}
