474be5dbaea56d112537ca9da11bd341c725c9f7
mahmoudmohamed <mahmoudmohamed01559@gmail.com>
2026-04-15 03:22:53 +0200

    feat: initialize project structure with main entry point, routing, and assets

--- lib/core/router/app_router.dart
+++ lib/core/router/app_router.dart
@@ -2,21 +2,19 @@ import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:imposter/core/router/app_routes.dart';
 import 'package:imposter/core/router/app_transitions.dart';
-import 'package:imposter/features/splash/views/splash_view.dart';
-import 'package:imposter/features/home/views/home_view.dart';
-import 'package:imposter/features/game/views/topics_selection_view.dart';
 import 'package:imposter/features/game/views/game_settings_view.dart';
 import 'package:imposter/features/game/views/game_view.dart';
+import 'package:imposter/features/game/views/topics_selection_view.dart';
+import 'package:imposter/features/home/views/home_view.dart';
+import 'package:imposter/features/splash/views/splash_view.dart';
 
 final GoRouter appRouter = GoRouter(
   initialLocation: AppRoutes.splash,
-
   routes: [
     GoRoute(
       path: AppRoutes.splash,
       name: AppRoutes.splash,
       pageBuilder: (context, state) => AppTransitions.fade(
-        context: context,
         state: state,
         child: const SplashView(),
       ),
@@ -24,47 +22,25 @@ final GoRouter appRouter = GoRouter(
     GoRoute(
       path: AppRoutes.home,
       name: AppRoutes.home,
-      pageBuilder: (context, state) => AppTransitions.slideFromRight(
-        context: context,
-        state: state,
-        child: const HomeView(),
-      ),
+      builder: (context, state) => const HomeView(),
     ),
     GoRoute(
       path: AppRoutes.topicsSelection,
       name: AppRoutes.topicsSelection,
-      pageBuilder: (context, state) => AppTransitions.slideFromRight(
-        context: context,
-        state: state,
-        child: const TopicsSelectionView(),
-      ),
+      builder: (context, state) => const TopicsSelectionView(),
     ),
     GoRoute(
       path: AppRoutes.gameSettings,
       name: AppRoutes.gameSettings,
-      pageBuilder: (context, state) => AppTransitions.slideFromRight(
-        context: context,
-        state: state,
-        child: const GameSettingsView(),
-      ),
+      builder: (context, state) => const GameSettingsView(),
     ),
     GoRoute(
       path: AppRoutes.game,
       name: AppRoutes.game,
-      pageBuilder: (context, state) => AppTransitions.slideFromRight(
-        context: context,
-        state: state,
-        child: const GameView(),
-      ),
+      builder: (context, state) => const GameView(),
     ),
   ],
-  errorPageBuilder: (context, state) => AppTransitions.fade(
-    context: context,
-    state: state,
-    child: const Scaffold(
-      body: Center(
-        child: Text('Error'),
-      ),
-    ),
+  errorBuilder: (context, state) => const Scaffold(
+    body: Center(child: Text('Error')),
   ),
 );
