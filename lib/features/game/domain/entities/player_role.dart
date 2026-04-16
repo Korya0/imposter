8d5dc773fce04ae9b9875206290121036a9bfcf0
mahmoudmohamed <mahmoudmohamed01559@gmail.com>
2026-04-16 14:59:14 +0200

    refactor: remove unused files and associated references

--- lib/features/game/domain/entities/player_role.dart
+++
@@ -1,4 +0,0 @@
-enum PlayerRole {
-  citizen,
-  imposter,
-}
--- lib/features/game/domain/entities/word_entity.dart
+++
@@ -1,9 +0,0 @@
-class WordEntity {
-  final String id;
-  final String name;
-
-  const WordEntity({
-    required this.id,
-    required this.name,
-  });
-}
