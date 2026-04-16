0bc7c364cdb6c7781b58b47665e300b25614aa46
mahmoudmohamed <mahmoudmohamed01559@gmail.com>
2026-04-16 15:04:01 +0200

    refactor: remove unused legacy configuration files

--- lib/core/error/exceptions.dart
+++
@@ -1,9 +0,0 @@
-class AssetsException implements Exception {
-  final String message;
-  AssetsException(this.message);
-}
-
-class ParsingException implements Exception {
-  final String message;
-  ParsingException(this.message);
-}
