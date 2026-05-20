755d3c5610d3497e10b86c23bbc6065cadc132f3
mahmoudmohamed <mahmoudmohamed01559@gmail.com>
2026-05-21 01:34:17 +0300

    feat: implement HowToPlayView screen with dynamic rule section rendering

--- lib/features/how_to_play/presentation/views/how_to_play_view.dart
+++ lib/features/how_to_play/presentation/views/how_to_play_view.dart
@@ -5,7 +5,6 @@ import 'package:imposter/core/constants/app_strings.dart';
 import 'package:imposter/core/presentation/widgets/app_back_button.dart';
 import 'package:imposter/core/presentation/widgets/app_gap.dart';
 import 'package:imposter/core/style/fonts/app_text_styles.dart';
-import 'package:imposter/core/utils/build_context_extension.dart';
 import 'package:imposter/features/how_to_play/data/how_to_play_data.dart';
 import 'package:imposter/features/how_to_play/presentation/widgets/how_to_play_section_widget.dart';
 
@@ -60,10 +59,9 @@ class HowToPlayView extends StatelessWidget {
             ),
           ),
 
-          if (context.isLandscape)
-            const SliverToBoxAdapter(
-              child: AppGap(40),
-            ),
+          const SliverToBoxAdapter(
+            child: AppGap(40),
+          ),
         ],
       ),
     );
